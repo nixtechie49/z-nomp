9b2903635bed4437b557f2e7c11880b701098043
Jon Layton <me@jonl.io>
2018-01-24 09:06:03 +0000

    CORS issue fixed, req.body still nil after body-parser

--- httpController.js
+++ httpController.js
@@ -3,12 +3,19 @@ const level = require('level')
 // This will create or open the underlying LevelDB store.
 var db = level('./masf-entries-db')
 
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors())
+// parse application/json
+app.use(bodyParser.json())
+
 var saveEntry = function(req, res) {
     // See if ip already logged; only log once (?)
-    db.get(req.ip, function (err, value) {
+console.log(req.body)
+    db.get(req.body.ip, function (err, value) {
       if (err.notFound) {
         var now = Date.now()
         db.put(req.body.ip, now, function(err) {
