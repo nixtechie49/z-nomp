6a98fb507ec62b28d599a8c42ef599a3dff7ef94
Jon Layton <me@jonl.io>
2018-01-24 07:48:31 +0000

    Fill in httpController method

--- httpController.js
+++ httpController.js
@@ -1,21 +1,22 @@
+//TODO express
+const level = require('level')
 
-//TODO require express, etc
+// Create our database
+// This will create or open the underlying LevelDB store.
+var db = level('./masf-entries-db')
 
-var level = require('level')
+//This needs to be a controller method inside express
+exports.saveEntry = function(req, res) {
+    // See if ip already logged ; only log once (?)
+    db.get(req.ip, function (err, value) {
+      if (err.notFound) {
+        db.put(req.body.ip, Date.now(), function(err) {
+          if (err) return console.log('Ooops!', err) // some kind of I/O error
+          console.log('Yay timestamped: ', req.body.ip)
+        })
+        return res.status(500).send({err: err})
+      }
 
-// 1) Create our database, supply location and options.
-//    This will create or open the underlying LevelDB store.
-var db = level('./mydb')
-
-// 2) Put a key & value
-db.put('name', 'Level', function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
-
-  // 3) Fetch by key
-  db.get('name', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
-
-    // Ta da!
-    console.log('name=' + value)
-  })
-})
+      return res.status(500).send({err: err})
+    })
+}
